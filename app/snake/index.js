4235e14e96c8b7034c46ba4f97a9657e7df3b93d
Karlili <karlili.li@gmail.com>
2017-04-20 15:53:38 +0800

    Adding comments to the index.js

--- app/snake/index.js
+++ app/snake/index.js
@@ -1,4 +1,7 @@
 const s = require('./createSnake');
 const keyboardListener = require('./interfactions/keyboardListener');
+
+//to initialize the environemnt,
+//define the snake body length, starting position of X and Y on the screen
 s.initializeEnvironment(10, 300, 10);
 
